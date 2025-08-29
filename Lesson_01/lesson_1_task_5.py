d1dbab9d9ec6fbed97b3b2491cd9e08b23c39b5e
Senior Pablo <Pavel_DF@mail.ru>
2025-08-29 19:51:03 +0300

    first commit

---
+++ Lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_par(num):
+    print(num, end='')
+
+
+print_par(8)
+print_par(8)
+print_par(0)
+print_par(0)
+print_par(5)
+print_par(5)
+print_par(5)
+print_par(3)
+print_par(5)
+print_par(3)
+print_par(5)
