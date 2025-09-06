01d9b51ad72aa53cdf3bf0f3007eb2dc02d2c3e3
Senior Pablo <Pavel_DF@mail.ru>
2025-09-06 13:40:00 +0300

    first commit

---
+++ Ex1_files/Training/Training_1.py
@@ -0,0 +1,26 @@
+# my_heigh = 200
+# print(my_heigh)
+
+# my_name = "Mister"
+# my_name = "Mister Sam"
+# print(my_name)
+
+# pet_name = input("What is your pet's name?")
+# print  ("Your lovely pet - " + pet_name)
+
+# def print_python():
+    # print  ("Doing Python excercises!")
+
+# print_python()
+
+def print_par(let):
+    print(let, end='')
+
+
+print_par('S')
+print_par('t')
+print_par('u')
+print_par('d')
+print_par('e')
+print_par('n')
+print_par('t')
